db588cd71d2e5f905414e0d0927b36f9e692953e
ccm03 <cmorri72@msudenver.edu>
2024-09-18 20:49:29 +0000

    ge03models Add Student

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
-class StudentTest < ActiveSupport::TestCase
+  test "should raise error when saving student without first name" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(last_name: "Nikola", school_email: "jokic@msudenver.edu", major: "CS")
+    end
+  end
+
+end
+
+# class StudentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-end
+# end
