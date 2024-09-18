0512e0af18edeb1d594999cf9c9487fc13b68e0d
ccm03 <cmorri72@msudenver.edu>
2024-09-18 20:53:39 +0000

    ge03models Student Model Test Corrected

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
   test "should raise error when saving student without first name" do
     assert_raises ActiveRecord::RecordInvalid do
       Student.create!(last_name: "Nikola", school_email: "jokic@msudenver.edu", major: "CS")
-    end
+    
   end
 
 end
